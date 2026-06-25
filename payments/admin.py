cb2f79ddef1f32e504bec18a8fd845ff88be907a
Abdiel Garcia <abdygaro2@hotmail.com>
2026-06-25 17:45:54 -0600

    add payment model

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+# This will help us to see the payments using the admin panel
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "ticket",
+        "amount",
+        "payment_method",
+        "cash_day",
+        "received_by_employee",
+        "created_at",
+    )
+
+    list_filter = (
+        "payment_method",
+        "cash_day",
+        "created_at",
+    )
+
+    search_fields = (
+        "ticket__ticket_number",
+        "ticket__vehicle_plate",
+        "sinpe_reference",
+    )
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,57 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from cash.models import CashDay
+from tickets.models import Ticket
+
+
+class Payment(models.Model):
+    CASH = "cash"
+    SINPE = "sinpe"
+
+    PAYMENT_METHOD_CHOICES = [
+        (CASH, "Cash"),
+        (SINPE, "SINPE"),
+    ]
+# this will connect the payment with an specific ticket
+# we use OneToOneFiled because the ticket can have only 1 payment
+    ticket = models.OneToOneField(
+        Ticket,
+        on_delete=models.PROTECT,
+        related_name="payment",
+    )
+# this will connect the payments with the cash day
+    cash_day = models.ForeignKey(
+        CashDay,
+        on_delete=models.PROTECT,
+        related_name="payments",
+    )
+# this will save which employee made the collection
+    received_by_employee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="received_payments",
+    )
+# the method of payment (sinpe/cash)
+    payment_method = models.CharField(
+        max_length=30,
+        choices=PAYMENT_METHOD_CHOICES,
+    )
+# total amount of the payment
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+    )
+# sinpe movil reference(this will be optional)
+    sinpe_reference = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Payment for {self.ticket.ticket_number} - ₡{self.amount}"
